0222ee8cfd686ed0166678db85d528b03b09368d
Natalia Łączkowska <91725304+laczkowska@users.noreply.github.com>
2024-01-19 14:19:57 +0100

    Add files via upload

--- library/library/src/main/java/com/example/library/mappery/Mapper.java
+++ library/library/src/main/java/com/example/library/mappery/Mapper.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Mapper {
-    private final ModelMapper modelMapper;
 
     public Mapper(ModelMapper modelMapper) {
         this.modelMapper = modelMapper;
