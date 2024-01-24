48912d8e92303ee61ecc2eb817136b867909d6d9
Natalia Łączkowska <91725304+laczkowska@users.noreply.github.com>
2024-01-24 18:54:04 +0100

    Add files via upload

--- natalia-main/natalia-main/projekt/src/main/java/org/example/service/BookService.java
+++ natalia-main/natalia-main/projekt/src/main/java/org/example/service/BookService.java
@@ -3,7 +3,6 @@ package org.example.service;
 import jakarta.transaction.Transactional;
 import org.example.data.Book;
 import org.example.dto.BookDTO;
-import org.example.dto.BookMapper;
 import org.example.exception.ResourceNotFoundException;
 import org.example.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,38 +11,34 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.modelmapper.ModelMapper;
+
 @Service
 @Transactional
 public class BookService {
-
     private final BookRepository bookRepository;
-    private final BookMapper bookMapper;
+    private final ModelMapper modelMapper;
 
     @Autowired
-    public BookService(BookRepository bookRepository, BookMapper bookMapper) {
+    public BookService(BookRepository bookRepository, ModelMapper modelMapper) {
         this.bookRepository = bookRepository;
-        this.bookMapper = bookMapper;
+        this.modelMapper = modelMapper;
     }
+
     public BookDTO getBookById(Long id) {
         Book book = bookRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Nie znaleziono książki o ID: " + id));
-        return bookMapper.bookToBookDTO(book);
+        return modelMapper.map(book, BookDTO.class);
     }
+
     public List<BookDTO> getAllBooks() {
         List<Book> books = bookRepository.findAll();
         if (books.isEmpty()) {
             throw new ResourceNotFoundException("Brak dostępnych książek.");
         }
-        return books.stream().map(bookMapper::bookToBookDTO).collect(Collectors.toList());
+        return books.stream()
+                .map(book -> modelMapper.map(book, BookDTO.class))
+                .collect(Collectors.toList());
     }
-
-    public List<BookDTO> getAllBooksDTO() {
-        List<Book> books = bookRepository.findAll();
-        if (books.isEmpty()) {
-            throw new ResourceNotFoundException("Brak dostępnych książek.");
-        }
-        return books.stream().map(bookMapper::bookToBookDTO).collect(Collectors.toList());
-    }
-
-
 }
+
