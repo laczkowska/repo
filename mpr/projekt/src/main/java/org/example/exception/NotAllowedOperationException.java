5f47cec34e5094b10466b2fa99aa6d91dd90b79e
Natalia Łączkowska <91725304+laczkowska@users.noreply.github.com>
2024-01-25 12:57:13 +0100

    Delete mpr/projekt directory

--- mpr/projekt/src/main/java/org/example/exception/NotAllowedOperationException.java
+++
@@ -1,8 +0,0 @@
-package org.example.exception;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.FORBIDDEN)
-public class NotAllowedOperationException extends RuntimeException{
-}
